8c4048b11400ee3d8dab06ea03cffdc761e5daf1
irgifahlevi <irgifahlevi5@gmail.com>
2023-04-28 21:43:17 +0700

    Error handling spesifik

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.Write("Enter a number : ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter another number : ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(num1 / num2);
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
+
